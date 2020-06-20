0afcfe93e88a4664cee5192f930b2565b662cfbc
Dipendra Shrestha <dipendrashrestha059@gmail.com>
2020-06-20 19:31:07 +0545

    Add solution for maximum value of the loot

--- week-3/maximum-value-of-the-loot/main.cpp
+++ week-3/maximum-value-of-the-loot/main.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <functional>
+
+bool sort_by_value_per_weight(const std::pair<int, int> &a, const std::pair<int, int> &b)
+{
+    return (a.first / (double)a.second) > (b.first / (double)b.second);
+}
 
 double get_optimal_value(int capacity, std::vector<int> weights, std::vector<int> values)
 {
     double value = 0.0;
+    int n = weights.size();
 
-    std::vector<double> value_per_weights(weights.size());
+    std::vector<std::pair<int, int>> value_weight_pairs;
 
-    for (int i = 0; i < weights.size(); ++i)
+    for (int i = 0; i < n; i++)
     {
-        value_per_weights[i] = values[i] / (double)weights[i];
+        value_weight_pairs.push_back(std::make_pair(values[i], weights[i]));
     }
 
-    std::sort(value_per_weights.begin(), value_per_weights.end());
+    std::sort(value_weight_pairs.begin(), value_weight_pairs.end(), sort_by_value_per_weight);
+
+    for (int i = 0; i < n; i++)
+    {
+        if (capacity == 0)
+            return value;
+
+        int current_capacity = std::min(value_weight_pairs[i].second, capacity);
+        capacity -= current_capacity;
+        value += current_capacity * (value_weight_pairs[i].first / (double)value_weight_pairs[i].second);
+    }
 
     return value;
 }
